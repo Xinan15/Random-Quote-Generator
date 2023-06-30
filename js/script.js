bd38fd3026d3ed7560dbcd40ae532ae8fc263951
Xinan <youkingan11@gmail.com>
2023-06-30 11:29:32 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+import { quotes } from './quotes.js';
+
+//Function to randomly select a quote value and return a random quote object from the quotes array
+function getRandomQuote () {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	var randomQuote = quotes[randomNumber];
+	return randomQuote;
+}	
+
+//Function to select random rgb color value
+function getRandomColor () {
+	var red = Math.floor(Math.random() * 256 );
+	var green = Math.floor(Math.random() * 256 );
+	var blue = Math.floor(Math.random() * 256 );
+	 var randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	return randomColor;
+}
+
+//Function to call the getRandomQuote function and stores the returned quote object in a variable
+//Constructs a string containing the different properties of the quote object 
+function printQuote () {
+		var quotes = getRandomQuote ();
+		var quoteContainer = document.getElementById("quote-box");
+		var quoteString = `<p class="quote">${quotes.quote}</p><p class="source">${quotes.source}`;
+				if (quotes.citation) {quoteString += `<span class="citation">${quotes.citation}</span>`}
+				if (quotes.year) {quoteString += `<span class="year">${quotes.year}</span></p>`}
+				else {quoteString += '</p>'};
+					quoteContainer.innerHTML = quoteString;
+
+					//assigns random color value to document background color 
+					document.body.style.backgroundColor = getRandomColor ();
+}
+
+//Quote automatically refreshes every 15 seconds
+window.setInterval(function(){
+printQuote ();
+}, 15000);
+
+//Event listener on LoadQuote button to generate new quote		
+document.getElementById("loadQuote").addEventListener("click", printQuote, false);
